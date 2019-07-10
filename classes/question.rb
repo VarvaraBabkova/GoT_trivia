66f121d5e03e8bfd37a6054fd2ab5db70bdc7007
Varvara Babkova <varvara.munday@gmail.com>
2019-07-10 13:37:48 -0500

    removed empty named characters

--- classes/question.rb
+++ classes/question.rb
@@ -1,8 +1,20 @@
 class Question
 
-	def ask_question(prompt)
-		
-
+	def initialize
+		@pastel = Pastel.new
 	end
 
+	def ask_question(prompt)
+	end
+
+	def correct_answer(str)
+		puts @pastel.green(str)
+	end
+
+	def not_correct_answer(str)
+		puts @pastel.red(str)
+	end
+
+	def prompt
+	end
 end
