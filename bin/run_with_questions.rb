972fcf27bc16332852624c7b563fca4f6d813ea4
Varvara Babkova <varvara.munday@gmail.com>
2019-07-10 12:52:17 -0500

    bets with sliders

--- bin/run_with_questions.rb
+++ bin/run_with_questions.rb
@@ -13,34 +13,48 @@ def main
 
 	while true
 
-		number_of_questions_in_quiz = 5
-		questions_type = [BookQuestion, CharacterAppearanceQuestion]
-		account = 10
+		number_of_questions_in_quiz = 15
+		initial_account = 10
+		questions_type = [BookQuestion, CharacterAppearanceQuestion, CharacterCultureQuestion]
+		account = initial_account
 
 		questions_array = []
 		answers_array = []
 
 		#randomly forming array of questions
 		number_of_questions_in_quiz.times do |i|
-			questions_array << questions_type[rand(questions_type.size)].new
+			questions_array << questions_type.sample.new
 		end
 
 		#actually asking
 		questions_array.each do |q|
+
+			if (account == 0) 
+				puts "You have nothing to play with!"
+				break
+			end
 			bet = make_a_bet(prompt, account)
-			puts "Your bet is " + bet.to_s
+			
 
 			account -= bet
 
 			answers_array << q.ask_question(prompt)
 
 			account += bet*2 if answers_array.last
-			puts "Your account is " + account.to_s
+			puts "Your account is " + account.to_s + " now."
 		end
 
-		puts
-		puts "Your answered " + answers_array.count(true).to_s + " times correctly out of " + number_of_questions_in_quiz.to_s
 
+		#the end of question set
+		puts
+		if (account > initial_account)
+				puts "Your account is " + account.to_s + " and you gained " + ( - initial_account + account).to_s + " Gold Dragon coins!"
+			else
+				puts "Your account is " + account.to_s
+			end
+
+		puts "Your answered " + answers_array.count(true).to_s + " times correctly out of " + number_of_questions_in_quiz.to_s
+		
  		answer = prompt.select("Do you wanna try another time?", %w(Yes No))
 
  		if answer == "No"
