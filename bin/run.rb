e022f8b9492853a9b9f4b1122bfe397dd85cd852
github username <github email address>
2019-07-10 10:52:28 -0500

    updated q2 and q3 for random answers

--- bin/run.rb
+++ bin/run.rb
@@ -60,8 +60,20 @@ end
 
 
 def q2(prompt)
+    arr = [1,2,3,4,5,6,7,8,9]
     random_id = rand(Character.last.id - Character.first.id) + Character.first.id
-    answer2 = prompt.select("How many books does #{Character.name_by_id(random_id)} appear in?", [Character.how_many_books_character_appeares_in(random_id), 4, 6, 8])
+    correct_ans = Character.how_many_books_character_appeares_in(random_id)
+    ans = [correct_ans]
+    num = 0
+    while num < 4
+        i=rand(arr.length)
+        if !ans.include? (arr[i])
+            ans << arr[i]
+            num += 1
+        end
+    end
+
+    answer2 = prompt.select("How many books does #{Character.name_by_id(random_id)} appear in?", ans.shuffle)
     if answer2 == Character.how_many_books_character_appeares_in(random_id)
         puts "Correct " + Character.how_many_books_character_appeares_in(random_id).to_s
     else
